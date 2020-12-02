2270a5d9a941d7e47d127ee93046719881d677bd
AlenDumas <ardumas@uri.edu>
2020-12-02 01:26:01 +0000

    start of boyer moore

--- BMoore.cpp
+++ BMoore.cpp
@@ -1,23 +1,59 @@
 #include "StringSearch.h"
 
-void badChar(){
+// void Search::preprocess(int *shift, int* pos, char *word, int size){
 
-    //iterate through the length of the pattern
-        //set the bad character to -1 (we'll have an aray full of -1)
-    //iterate through the length of the text
-        //set the badchar to the last occurance of a char
+//     word[size] = size + 1;
+
+//     while(size > 0){
+//         while(pos + 1 <= size && word[size-1] != word[pos-1]){
+
+//             if(shift[pos] == 0)
+//                 shift[pos] = pos-size
+
+//             pos = word[pos]
+//         }
+
+//         size--;
+//         pos--;
+//         word[size] = pos;
+//     }
+// }
+
+void Search::badChar(std::string text, int size, int badChar[table]){
+     
+    int table = 256; //represents ascii table
+    int i;
+
+    for(i = 0; i< table ; i++)
+        badChar[i] = -1;
+
+    for(i = 0; i < size; i++)
+        badChar[(int) text[i]] = i
 }
 
-void goodChar(){
+// void goodChar(){
 
-    //do the stuff
-}
+//     //do the stuff
+// }
 
-void search(){
-
-    //get length of pattern
-    //length of text
-
-    //call the bad char array
-
+void Search::BM(std::string wordInText, std::string text){
+    
+    std::vector<int>positions;
+    int badChar[256];
+    int shift = 0;
+    
+    badChar(wordInText, text.size, badchar)
+    
+    while(shift < = (text.size() - wordInText.size()){
+        int j = wordInText.size() -1;
+        
+        while(j > 0 && wordInText[j] == text[size + j]){
+            j--
+        }
+        
+        if(j < 0 ){
+            positions.push_back(shift)
+        }
+        
+    }
 }
--- StringSearch.h
+++ StringSearch.h
@@ -7,11 +7,12 @@ class Search{
     private:
 
         std::string wordInText;
-        std::string text;
+        std::string text;//maybe make this a vector
 
     public:
         std::vector<int> KMP(std::string wordInText, std::string text);
         void KMPRecursive(std::string wordInText, std::string text, int i, int j, std::vector<int> &positions, std::vector<int> lpsVec);
         void getLPS(std::vector<int> &lpsVec, std::string wordInText);
         std::vector<int> BM(std::string wordInText, std::string text);
+        void badChar(std::string text, int size, int badChar[table]);
 };
