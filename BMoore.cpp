55461c56f29482f7ab7490dbcf8fdc6a69261f85
AlenDumas <ardumas@uri.edu>
2020-12-13 05:08:03 +0000

    update to Boyer Moore and header file

--- BMoore.cpp
+++ BMoore.cpp
@@ -1,71 +1,109 @@
 #include "StringSearch.h"
 
-//get the shift for good suffix rule
-int Search::goodSuffix(int j, std::string& wordInText){
-    int endPos = wordInText.length() - 1;
-    int index = -1;
-    j--;
 
-    while(j >= 0){
-        if(wordInText[j] == wordInText[endPos]){
-            index = j;
-            break;
-        }
-        else
-            j--;
+
+// get the shift for good suffix rule
+std::vector<int> Search::goodSuffix(std::string wordInText, std::vector<int> &shiftTable, std::vector<int> &borderTable) {
+    int i = wordInText.length();
+    int j = wordInText.length() + 1;
+
+    for (int i = 0; i < wordInText.length() + 1; i++) {
+        shiftTable.push_back(0); // filling the shift table with all zero values
+        borderTable.push_back(-1); // filling the border position table with -1
     }
-    return endPos - index;
+
+    borderTable[i] = j; // initalizing the border table to the end of the pattern
+
+    while (i > 0) {
+        // if the current character at pos i-1 is not a match to the character at j-1, search the rightmost position on the border table
+        while (j <= wordInText.length() && wordInText[i - j] != wordInText[j - 1]) {
+            // shift the shift the pattern from i to j
+            if (shiftTable[j] == 0) {
+                shiftTable[j] = j - i;
+            }
+
+            // update the position of the next border
+            j = borderTable[j];
+        }
+
+        // When the border is found, store the begining position of the border
+        i--;
+        j--;
+        borderTable[i] = j;
+    }
+
+    j = borderTable[0];
+    for (i = o; i <= wordInText.length(); i++) {
+        // if the shift has not been set yet
+        if (shiftTable[i] == 0) {
+            shiftTable[i] = j;
+        }
+
+        if (i == j) {
+            // move to the next border
+            j = borderTable[j];
+        }
+    }
 }
 
-//get shift for bad character rule
-int Search::badChar(int j, char temp, std::string WordInText){ //we're only comparing one car of the text
-    int index = -1;
+// returns an array of the locations for mismatches
+std::vector<int> Search::badChar(std::string wordInText) { //we're only comparing one car of the text
+    std::vector<int> badCharTable;
+    int size = 256; // number of possible ASCII values
 
-    for(int i = j - 1; i >= 0; --i){
-        if(WordInText[i] == temp){
-            index = i;
-            break;
-        }
+    for (int i = 0; i < 256; i++) {
+        badCharTable.push_back(-1); // filling the table with negative values that will be replaced later
     }
-    return j - index;
+    for (int i = 0; i < wordInText.length(); i++) {
+        badCharTable[(int)wordInText[i]] = i; // pushing the last occurance of a particular character in the patterm
+    }
+
+    return badCharTable;
 }
 
-std::vector<int> Search::BM(std::string wordInText, std::string text){
+// function that returns the maximum value between two integers
+int max(int a, int b) {
+    return a >= b ? a : b;
+}
+
+std::vector<int> Search::BM(std::string wordInText, std::string text) {
     std::vector<int> positions;
-    int mismatch = 0;
 
-    if(text.length() < wordInText.length()){
+    if (text.length() < wordInText.length()) {
         return positions;
     }
 
-    int i = wordInText.length() - 1; //begin match from the end
-    int j = text.length() -1;
+    int mismatch = 0;
 
-    while(i < wordInText.length()){ //is there is a successful match
-        if(text[i] == wordInText[j]){
-            if(j == 0){
-                positions.push_back(i);
-            }
-            i--;
+    //initializing vectors to hold shift values
+    std::vector<int> badCharTable = badChar(wordInText);
+
+    std::vector<int> shiftTable;
+    std::vector<int> borderTable;
+
+    goodSuffix(wordInText, shiftTable, borderTable);
+
+    int i = 0;
+    int len_word = wordInText.length();
+    int len_text = text.length();
+
+    while (i <= (len_text - len_word)) {
+        int j = len_word - 1;
+        while (j >= 0 && (text[i + j] == wordInText[j])) {
+            // looking for a sutiable suffix match
             j--;
         }
-
-        else{ // there is no match
-            if(j == wordInText.length()-1){ //if the last character is not matched, apply bad character rule
-                mismatch = badChar(j, text[i], wordInText);
-                i = i + wordInText.length() - 1 - j + mismatch;
-                j = wordInText.length();
-            }
-
-            else{//if there is a suffix, compare bad character and good suffix rules
-                mismatch = badChar(j, text[i], wordInText);
-                if(mismatch == -1){
-                    mismatch = wordInText.length();
-                    i = i + wordInText.length() - 1 - j + mismatch;
-                    j = wordInText.length();
-                }
-            }
-
+        if (j < 0) { // match found
+            positions.push_back(i);
+            i += shiftTable[0]; //implement an appropriate shift
+        }
+        else {
+            int badCharOffset = badCharTable[text[i + j]];
+            int goodSuffixOffset = shiftTable[j + i];
+            // implement either the good suffix or bad character rule depending on which produces a larger shift
+            i += max(goodSuffixOffset, j - badCharOffset);
         }
     }
+
+    return positions;
 }
--- StringSearch.h
+++ StringSearch.h
@@ -1,22 +1,21 @@
 #include <iostream>
+#include <algorithm>
 #include <string>
 #include <vector>
 
-class Search{
+class Search {
 
-    private:
+private:
 
-        std::string wordInText; //(pattern)
-        std::string text;
-        void KMPRecursive(std::string wordInText, std::string text, int i, int j, std::vector<int> &positions, std::vector<int> lpsVec);
-        void KMPLinear(std::string wordInText, std::string text, int i, int j, std::vector<int> &positions, std::vector<int> lpsVec);
-        void getLPS(std::vector<int> &lpsVec, std::string wordInText);
+    std::string wordInText; //(pattern)
+    std::string text;
+    void KMPRecursive(std::string wordInText, std::string text, int i, int j, std::vector<int>& positions, std::vector<int> lpsVec);
+    void KMPLinear(std::string wordInText, std::string text, int i, int j, std::vector<int>& positions, std::vector<int> lpsVec);
+    void getLPS(std::vector<int>& lpsVec, std::string wordInText);
+    std::vector<int> goodSuffix(std::string wordInText, std::vector<int>& shiftTable, std::vector<int>& borderTable);
+    std::vector<int> badChar(std::string wordInText);
 
-    public:
-        std::vector<int> KMP(std::string wordInText, std::string text,std::string linearOrRecursive);
-
-        std::vector<int> BM(std::string wordInText, std::string text);
-        int badChar(int j, char temp, std::string WordInText);
-        int goodSuffix(int j, std::string& wordInText);
-
+public:
+    std::vector<int> KMP(std::string wordInText, std::string text, std::string linearOrRecursive);
+    std::vector<int> BM(std::string wordInText, std::string text);
 };
